de431b96f1120c6cf640606e9b43c05bfe603853
istiklal gunes <60663260+istiklalg@users.noreply.github.com>
2022-03-08 19:48:02 +0300

    Update ULAK_APIS.py

--- ulak_root/ULAK_APIS.py
+++ ulak_root/ULAK_APIS.py
@@ -10,7 +10,7 @@ class UlakSearchEngine:
         self.search_engine_id = _search_engine_id
 
 
-#s2 = UlakSearchEngine("WWWkloım95535443reomervle", "55555sdfsdfs555")
+#s2 = UlakSearchEngine(_api_key, _search_engine_id)
 
 # decorator function for object error handler
 def engine_test(func):
@@ -26,30 +26,8 @@ def engine_test(func):
 @engine_test
 def view(engine, num):
     print(str(engine.search_engine_id)+" "+str(num))
-    print(f" Bu nesne için süreç ilerledi ")
-    
+   
 
 # view(s1, 7645)
 #view(s2, 16352)
 #view("bu NE", 5)
-
-
-        
-            
-
-"""
-# an example for search function creation:
-------------------------------------------
-from googleapiclient.discovery import build
-
-my_api_key = "YOUR_API_KEY"
-my_cse_id = "YOUR_CSE_ID"
-
-def google_results_count(query):
-    service = build("customsearch", "v1",
-                    developerKey=my_api_key)
-    result = service.cse().list(q=query, cx=my_cse_id, sort="date:r:20110101:20131231").execute()
-    return result["searchInformation"]["totalResults"]
-
-print google_results_count('arab spring site:www.cnn.com')
-"""
