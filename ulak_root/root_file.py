4745580730ef39b1957cf6c306167e9063f8e0ce
istiklal gunes <60663260+istiklalg@users.noreply.github.com>
2022-03-08 20:09:15 +0300

    Update root_file.py

--- ulak_root/root_file.py
+++ ulak_root/root_file.py
@@ -20,16 +20,12 @@ ulak_1 = UlakSearchEngine(istiklal_api_key, ulak_1_search_engine_id)
 ulak_2 = UlakSearchEngine(istiklal_api_key, ulak_2_search_engine_id)
 ulak_3 = UlakSearchEngine(istiklal_api_key, ulak_3_search_engine_id)
 
-# to create and use more than one search engine
 engine_list = [ulak_1, ulak_2, ulak_3]
 
-# backing up existing results.txt to old_results.txt and clean it for new search
 backup_and_clean_results("old_results.txt", "results.txt")
 
-# 90 day search limit for new search according to today
 sort_sentence = create_sort_sentence()
 
-# serching operations with two different search engine with different 10 + 7 sites
 for engine in engine_list:
     try:
         start_search(engine, date_sort_sentence=sort_sentence)
@@ -39,7 +35,6 @@ for engine in engine_list:
             exception.write(f"\n**({datetime.datetime.now().strftime('%d/%m/%Y, %H:%M:%S')}) - ulak_root calling start_search function - ({err}) - ({engine})\n")
         
 
-# mailing results to receipents in list
 try:
     despatch_rider(_content_file="results.txt")
 except Exception as err:
